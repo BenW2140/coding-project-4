277c0bc79bc137692ba4916ad71e24094f3908d3
Ben White <bwhite2140@outlook.com>
2020-06-19 09:03:51 -0700

    add pizza constructor and a prototype method for calculating the price

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
+function Pizza(size, toppings) {
+  this.size = size,
+  this.toppings = toppings
+}
 
+Pizza.prototype.calculatePrice = function(pizza) {
+  let price = 0;
+  if (pizza.size === "Large") {
+    price = 10;
+  } else if (pizza.slice === "Medium") {
+    price = 8;
+  } else {
+    price = 6;
+  }
+  if (pizza.toppings === "Sausage" || pizza.topping === "Pepperoni") {
+    price += 1;
+  }
+  return price;
+}
+
+$(document).ready(function() {
+  
 });
